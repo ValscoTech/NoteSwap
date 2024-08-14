5dfcf9dfeaa2d5d466c2eb226b400a1562514452
gautam-4 <gautamalt468@gmail.com>
2024-08-14 19:49:46 +0530

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import { HomePage, AccountPage, NotesPage, OfferNotesPage, RentNotesPage, BlogsP
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<><LoginPage /></>} />
-          <Route path="/home" element={<><HomePage /></>} />
-          <Route path="/account" element={<><AccountPage /></>} />
-          <Route path="/notes" element={<><NotesPage /></>} />
-          <Route path="/offer" element={<><OfferNotesPage /></>} />
-          <Route path="/rent" element={<><RentNotesPage /></>} />
-          <Route path="/blog" element={<><BlogsPage /></>} />
-        </Routes>
-      </BrowserRouter>
+      <div className='font-clash bg-black text-white min-h-screen'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<><LoginPage /></>} />
+            <Route path="/home" element={<><HomePage /></>} />
+            <Route path="/account" element={<><AccountPage /></>} />
+            <Route path="/notes" element={<><NotesPage /></>} />
+            <Route path="/offer" element={<><OfferNotesPage /></>} />
+            <Route path="/rent" element={<><RentNotesPage /></>} />
+            <Route path="/blog" element={<><BlogsPage /></>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        clash: ['ClashDisplay-Variable', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
