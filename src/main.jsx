a0f99e78e7834f836b470fe29e1b070b2fbd0b3c
gautam-4 <gautamalt468@gmail.com>
2024-08-16 23:12:10 +0530

    phone verification using clerk

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '@/styles/index.css'
+import { ClerkProvider, RedirectToSignIn } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const clerkFrontendApi = import.meta.env.VITE_CLERK_FRONTEND_API_URL;
+
+if (!clerkFrontendApi) {
+    throw new Error("Missing frontend api")
+  }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    {/* <ClerkProvider frontendApi={clerkFrontendApi} publishableKey={PUBLISHABLE_KEY}> */}
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
