91bec1d0631ecd29b7a40936550d2e149fc3073d
Nikhil1141 <yepuri.nikhileswar@gmail.com>
2024-08-21 23:53:22 +0530

    blog

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,7 +1,5 @@
 import Blog from '@/components/blogs/Blog'
 import BlogsCarousel from '@/components/blogs/BlogsCarousel'
-// import Footer from '@/components/layout/footer'
-// import Navbar from '@/components/layout/navbar'
 import { memo } from 'react'
 
 const BlogsPage = () => {
@@ -14,5 +12,3 @@ const BlogsPage = () => {
 }
 
 export default memo(BlogsPage)
-// <Footer />
-// <Navbar />
