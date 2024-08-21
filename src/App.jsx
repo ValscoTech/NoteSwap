a94b00199a18766ae3f0f530071ea5120331cb48
LIKITHA <bhaskarlikitha705@gmail.com>
2024-08-21 18:13:46 +0530

    editAccountpage edited

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage, AccountPage, NotesPage, OfferNotesPage, RentNotesPage, BlogsPage, LoginPage, EditAccountPage, SignUpPage } from '@/pages';
 import Navbar from '@/components/layout/navbar';
 import Footer from '@/components/layout/footer';
-import { SignedIn, SignedOut, SignInButton, UserButton, SignIn, SignUp } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <div className='font-clash bg-black text-white min-h-screen'>
-        <SignedOut>
-          <main className='h-screen flex items-center justify-center'>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/sign-up" element={<SignUp routing="hash" />} />
-                <Route path="/sign-in" element={<SignIn routing="hash" />} />
-              </Routes>
-            </BrowserRouter>
-          </main>
-        </SignedOut>
-        <SignedIn>
-          <Navbar />
-          <BrowserRouter>
-            {/* <UserButton/> */}
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/account" element={<AccountPage />} />
-              <Route path="/notes" element={<NotesPage />} />
-              <Route path="/offer" element={<OfferNotesPage />} />
-              <Route path="/rent" element={<RentNotesPage />} />
-              <Route path="/blog" element={<BlogsPage />} />
-              <Route path="/edit-account" element={<EditAccountPage />} />
-              <Route path="/signup" element={<SignUpPage />} />
-            </Routes>
-          </BrowserRouter>
-          <Footer />
-        </SignedIn>
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/notes" element={<NotesPage />} />
+            <Route path="/offer" element={<OfferNotesPage />} />
+            <Route path="/rent" element={<RentNotesPage />} />
+            <Route path="/blog" element={<BlogsPage />} />
+            <Route path="/edit-account" element={<EditAccountPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
       </div>
     </>
   );
