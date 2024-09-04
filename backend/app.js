21c9164c06786af59a64d5810743df02e5fcd3b7
Lonewolf230 <manish2306j@gmail.com>
2024-09-04 23:05:15 +0530

    Custom Auth, UserProfile,CourseNotes done along with some middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,35 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
+const authenticateUser=require('./utils/middleware').authenticateUser;
 const notesRouter=require('./controllers/notes');
 const profileRouter=require('./controllers/userprofile');
-const supabase=require('./supabaseclient');
+const authRouter=require('./controllers/customAuth');
+// const {supabase}=require('./supabaseclient');
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
+app.use('/api/auth',authRouter);    
+app.use('/api/notes',authenticateUser,notesRouter); 
+app.use('/api/profile',authenticateUser,profileRouter); 
 
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Route not found',
+    });
+});
 
 
-
-
-app.use('/api/notes',notesRouter);
-app.use('/api/profile',profileRouter);  
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        message: 'Internal Server Error',
+    });
+});
 
 
 
