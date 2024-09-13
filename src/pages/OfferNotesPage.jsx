f8a0a7adeb25e62bfbb87ba749d9d743f94e73dd
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-13 22:08:38 +0530

    Update OfferNotesPage.jsx

--- src/pages/OfferNotesPage.jsx
+++ src/pages/OfferNotesPage.jsx
@@ -140,7 +140,7 @@ export default function OfferNotesPage() {
             <div className="bg-[#A883C5] rounded-lg shadow-md p-6 flex flex-col justify-between">
               <div>
                 <h2 className="text-center text-xl font-bold text-black mb-4">Set Your Price</h2>
-                <input type="text" className="mt-10 w-full p-2 rounded-md border-b-2 border-black bg-transparent focus:outline-none" />
+                <input type="number" className="mt-10 w-full p-2 rounded-md border-b-2 border-black bg-transparent focus:outline-none" />
               </div>
               <button className="mt-4 w-full py-2 bg-[#DEEBFF] text-black font-normal rounded-md">Save Price</button>
             </div>
