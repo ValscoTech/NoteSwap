b4063ca27711024c94a50da767d12937124e8aca
Monish.S <150262983+Fishsticks53@users.noreply.github.com>
2024-09-14 18:19:00 +0530

    NotesView.jsx

--- src/components/common/NotesView.jsx
+++ src/components/common/NotesView.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
-import img3 from "../../assets/images/image3.png";
+import React, { useState, useEffect } from "react";
 import ImageViewer from "react-simple-image-viewer";
+import img3 from "../../assets/images/image3.png";
 import img4 from "../../assets/images/image4.png";
-import { useEffect } from "react";
 
-
-{/*Temporarily storing values locally */}
+// Default data
 const defaultData = [  
   {
     id: 1,
@@ -55,21 +53,18 @@ const defaultData = [
     school: "SCOPE",
     link: "/notes/computation-of-mathematics",
   },
-
 ];
 
-export default function NotesView({notesData = []}) {
+export default function NotesView({ notesData }) {
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [images, setImages] = useState([]);
-  const [data, setData] = useState(notesData.length > 0 ? notesData : defaultData);
+  const [data, setData] = useState(defaultData);
 
   useEffect(() => {
-    // Check if notesData is provided or empty, set to defaultData if not
-    if (notesData.length === 0) {
-      setData(defaultData);
-    } else {
-      setData(notesData);
+    // Prepend new notesData to the existing data
+    if (notesData && notesData.length > 0) {
+      setData((prevData) => [...notesData, ...prevData]);
     }
   }, [notesData]);
 
@@ -85,68 +80,63 @@ export default function NotesView({notesData = []}) {
 
   return (
     <div className="font-clash overflow-hidden">
-
-      {/* Displaying Filtered Results */}
       <div className="flex justify-center">
-            {/* Notes Display Section */}
-            <div className="w-[60rem]">
-              <div className="  grid justify-items-center sm:grid-cols-3 grid-cols-2 md:gap-y-14 lg:grid-cols-3 md:gap-x-[1rem] lg:gap-x-3 sm:gap-x-1 mobile:gap-y-10 mobile:gap-x-72 gap-x-[18rem] gap-y-[3rem] ">
-                {data.map((item, index) => (
-                  <a
-                    key={item.id}
-                    className="bg-white text-black p-3 rounded-2xl w-[18rem] h-[18rem] md:scale-100">
-                    {/* Component Part */}
-                    <div className="flex justify-normal gap-x-4 items-center">
-                      {/* Department + Year, Respective School */}
-                      <div className="pt-1 pl-2 font-[400] text-lg">
-                        {item.department}
-                      </div>
-                      <div className="bg-[#a883c5] px-5 py-0 h-4 rounded-[0.225rem] font-[500] text-sm flex flex-col justify-center">
-                        <div className="font-[525]">{item.school}</div>
-                      </div>
+        <div className="w-[60rem]">
+          <div className="grid justify-items-center sm:grid-cols-3 grid-cols-2 md:gap-y-14 lg:grid-cols-3 md:gap-x-[1rem] lg:gap-x-3 sm:gap-x-1 mobile:gap-y-10 mobile:gap-x-72 gap-x-[18rem] gap-y-[3rem]">
+            {data.map((item, index) => (
+              <a
+                key={item.id}
+                className="bg-white text-black p-3 rounded-2xl w-[18rem] h-[18rem] md:scale-100"
+              >
+                <div className="flex justify-normal gap-x-4 items-center">
+                  <div className="pt-1 pl-2 font-[400] text-lg">
+                    {item.department}
+                  </div>
+                  <div className="bg-[#a883c5] px-5 py-0 h-4 rounded-[0.225rem] font-[500] text-sm flex flex-col justify-center">
+                    <div className="font-[525]">{item.school}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center gap-x-2 pb-4 pl-0">
+                  <div className="text-[1.15rem] line-clamp-2 font-[425] w-[8.7rem]">
+                    {item.title}
+                  </div>
+                  <div className="flex justify-normal border-[1.25px] border-black rounded-[0.7rem] w-fit items-center p-2 gap-x-1">
+                    <div className="text-sm leading-4">
+                      Modules
+                      <br /> Covered
                     </div>
-                    <div className="flex justify-center gap-x-2 pb-4 pl-0">
-                      {/* Course and Modules Covered */}
-                      <div className="text-[1.15rem] font-[425] w-[8.7rem]">
-                        {item.title}
-                      </div>
-                      <div className="flex justify-normal border-[1.25px] border-black rounded-[0.7rem] w-fit items-center p-2 gap-x-1">
-                        <div className="text-sm leading-4">
-                          Modules
-                          <br /> Covered
-                        </div>
-                        <div className="text-4xl">{item.modulesCovered}</div>
-                      </div>
-                    </div>
-                    <div className="flex justify-normal">
-                      {/* Images Part */}
-                      <div>
-                        <img
-                          className="w-32 cursor-pointer"
-                          src={item.image1}
-                          onClick={() => openImageViewer(0, item)}
-                        />
-                      </div>
-                      <div>
-                        <img
-                          className="w-32 cursor-pointer"
-                          src={item.image2}
-                          onClick={() => openImageViewer(1, item)}
-                        />
-                      </div>
-                    </div>
-                  </a>
-                ))}
-              </div>
-            </div>
+                    <div className="text-4xl">{item.modulesCovered}</div>
+                  </div>
+                </div>
+                <div className="flex justify-normal">
+                  <div>
+                    <img
+                      className="w-32 cursor-pointer"
+                      src={item.image1}
+                      onClick={() => openImageViewer(0, item)}
+                    />
+                  </div>
+                  <div>
+                    <img
+                      className="w-32 cursor-pointer"
+                      src={item.image2}
+                      onClick={() => openImageViewer(1, item)}
+                    />
+                  </div>
+                </div>
+              </a>
+            ))}
           </div>
-          {isViewerOpen && (
+        </div>
+      </div>
+      {isViewerOpen && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80"
           style={{
-            width: "100vw", // Cover the entire screen width
-            height: "100vh", // Cover the entire screen height
-          }}>
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
           <ImageViewer
             src={images}
             className="min-w-full"
@@ -155,15 +145,14 @@ export default function NotesView({notesData = []}) {
             closeOnClickOutside={true}
             onClose={closeImageViewer}
             backgroundStyle={{
-              backgroundColor: "rgba(0, 0, 0, 0.9)", // Dark background
+              backgroundColor: "rgba(0, 0, 0, 0.9)",
               zIndex: 9999,
-              width: "100%", // Full width for the background
-              height: "100", // Full height for the background
+              width: "100%",
+              height: "100%",
             }}
           />
         </div>
       )}
-      {/*The footer part */}
     </div>
   );
 }
