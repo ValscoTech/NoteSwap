379c30d570e0aab5b39b31366c48d9470d0e2066
Nikhil1141 <yepuri.nikhileswar@gmail.com>
2024-09-14 21:56:19 +0530

    blog

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,10 +1,14 @@
 import Blog from '@/components/blogs/Blog'
 import BlogsCarousel from '@/components/blogs/BlogsCarousel'
-import { memo } from 'react'
+import { memo, useContext } from 'react'
+import "../styles/ThemeContext.css";
+import { ThemeContext } from './ThemeContext';
+
 
 const BlogsPage = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div>
+    <div className={theme}>
       <Blog />
       <BlogsCarousel />
     </div>
