7e244c5c58c4f6da1dc292ba3161cca379abd5f2
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-14 22:27:36 +0530

    Update OfferNotesPage.jsx

--- src/pages/OfferNotesPage.jsx
+++ src/pages/OfferNotesPage.jsx
@@ -198,16 +198,17 @@ export default function OfferNotesPage() {
                       Drag and drop your files here
                     </label>
                   </div>
+                  </div>
                   <div className="mt-4">
                     {files.length > 0 && (
                       <ul className="text-white list-disc pl-5">
                         {files.map((file, index) => (
-                          <li key={index} className="text-white">{file.name}</li>
+                          <li key={index} className="text-black font-clash font-bold">{file.name}</li>
                         ))}
                       </ul>
                     )}
                   </div>
-                </div>
+                
               </div>
             </div>
 
