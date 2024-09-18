05ff8a9bc80786dcf2dc0b243dd5db90bb8b29e6
Monish.S <150262983+Fishsticks53@users.noreply.github.com>
2024-09-18 22:43:00 +0530

    Update NotesPage.jsx

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,14 +1,21 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { GoArrowDownRight } from "react-icons/go";
-import ImageViewer from "react-simple-image-viewer";
+import { useNavigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
 import shelf from "../assets/images/shelf.png";
 import img3 from "../assets/images/image3.png";
 import img4 from "../assets/images/image4.png";
+import { GoArrowDownRight } from "react-icons/go";
+import ImageViewer from "react-simple-image-viewer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useCallback } from "react";
 import { ThemeContext } from "./ThemeContext";
 import "../styles/ThemeContext.css";
+import NotesView from "@/components/common/NotesView";
+import FullscreenViewer from "@/components/common/FullScreenViewer";
 
-// Temporarily storing values locally
+{
+  /*Temporarily storing values locally */
+}
 const data = [
   {
     id: 1,
@@ -76,6 +83,7 @@ export default function NotesPage() {
   const navigate = useNavigate();
   const query = useQuery();
   const [hasManualSearch, setHasManualSearch] = useState(false);
+  const [linkIndex,setLinkIndex]=useState("");
   const [isInitialLoad, setIsInitialLoad] = useState(true);
 
   useEffect(() => {
@@ -95,7 +103,8 @@ export default function NotesPage() {
       // Set the states only if necessary to avoid resetting values
       if (parsedTerms.minPrice !== undefined) setMinPrice(parsedTerms.minPrice);
       if (parsedTerms.maxPrice !== undefined) setMaxPrice(parsedTerms.maxPrice);
-      if (parsedTerms.selectedTypes !== undefined) setSelectedTypes(parsedTerms.selectedTypes);
+      if (parsedTerms.selectedTypes !== undefined)
+        setSelectedTypes(parsedTerms.selectedTypes);
 
       // Set the manual search flag after the initial state update
       setHasManualSearch(true);
@@ -103,10 +112,11 @@ export default function NotesPage() {
     }
   }, [query]);
 
-  const openImageViewer = (index, item) => {
+  const openImageViewer = (index, item,linkIndex) => {
     setImages([item.image1, item.image2]);
     setCurrentImageIndex(index);
     setIsViewerOpen(true);
+    setLinkIndex(linkIndex);
   };
 
   const closeImageViewer = () => {
@@ -159,39 +169,42 @@ export default function NotesPage() {
     const encodedParams = encodeURIComponent(JSON.stringify(searchParams));
     if (Object.keys(searchParams).length > 0) {
       // Navigate with query parameters
-      navigate('/notes?query=${encodedParams}');
+      navigate(`/notes?query=${encodedParams}`);
     } else {
       // Navigate without query if no filters
-      navigate('/notes');
+      navigate(`/notes`);
     }
   };
 
   return (
     <div
-      className={'font-clash overflow-hidden ${theme === "dark" ? "bg-black text-white" : "bg-white text-black"}'}
+      className={`font-clash overflow-hidden ${
+        theme === "dark" ? "bg-black text-white" : "bg-white text-black"
+      } `}
       onLoad={() => {
         setHasManualSearch(false);
-      }}
-    >
-      {/* The navigation bar */}
-      <div className="scale-[0.5] sm:scale-100 sm:pt-10 lg:mb-[-1rem]">
+      }}>
+      <div className=" flex justify-center lg:scale-100 md:scale-[0.76] sm:scale-[0.61] pt-10 mobile:scale-[0.45] scale-[0.4] mt-[-6rem] mobile:mt-0">
         {/* Search Box Section */}
-        <div className="flex justify-center lg:mt-0 mt-[-10rem]">
-          <div className="flex justify-center pb-20 pt-20">
-            <form onSubmit={(e) => handleSubmit(e)}>
+        <div className=" min-w-[60rem] lg:mt-0 mt-[-10rem]">
+          <div className="flex justify-center pb-20 pt-20 ">
+            <form onSubmit={handleSubmit}>
               <div
-                className={'max-w-[58.5rem] min-w-[37.875rem] bg-white text-black pl-12 lg:pr-8 pr-1 pb-10 rounded-2xl ${theme === "dark" ? "border-0" : "border-2 border-black"}'}
-              >
-                <div className="text-[3.1rem] font-[600] pt-3">Search Notes</div>
+                className={`bg-white text-black md:pl-12 pr-6 pb-12 rounded-2xl sm:pl-10 mobile:pl-16 pl-10 ${
+                  theme === "dark" ? "border-0" : "border-2 border-black"
+                } `}>
+                <div className="text-[3.1rem] font-[600] pt-3">
+                  Search Notes
+                </div>
                 <div className="flex justify-center gap-x-12">
-                  <div className="pt-12">
-                    <div className="text-2xl pb- 3 font-[300]">Price Range</div>
+                  <div className="pt-12 ">
+                    <div className="text-2xl pb-3 font-[300]">Price Range</div>
                     <div className="grid grid-rows-2 grid-cols-2 gap-x-16 pb-4">
-                      <div className="pb-1 text-xl">Min</div>
-                      <div className="pb-1 text-xl">Max</div>
+                      <div className="pb-1 text-2xl font-light">Min</div>
+                      <div className="pb-1 text-2xl font-light">Max</div>
                       <div>
                         <input
-                          className="bg-[#d9d9d9] h-11 rounded-lg w-32 lg:w-auto"
+                          className="bg-[#d9d9d9] h-11 rounded-lg md:w-fit object-scale-down"
                           type="text"
                           value={minPrice}
                           onChange={(e) => setMinPrice(e.target.value)}
@@ -199,14 +212,14 @@ export default function NotesPage() {
                       </div>
                       <div>
                         <input
-                          className="bg-[#d9d9d9] h-11 rounded-lg w-32 lg:w-auto"
+                          className="bg-[#d9d9d9] h-11 rounded-lg md:w-fit"
                           type="text"
                           value={maxPrice}
                           onChange={(e) => setMaxPrice(e.target.value)}
                         />
                       </div>
                     </div>
-                    <div className="pb-4 text-xl">Note Type</div>
+                    <div className="pb-4 text-2xl font-light">Note Type</div>
                     <div className="flex justify-normal gap-x-9 pb-4">
                       <div
                         className={`relative bg-[#d9d9d9] flex flex-col justify-end h-24 px-5 w-fit rounded-lg cursor-pointer ${
@@ -214,8 +227,7 @@ export default function NotesPage() {
                             ? "border border-black"
                             : ""
                         }`}
-                        onClick={() => handleCheckboxChange("class notes")}
-                      >
+                        onClick={() => handleCheckboxChange("class notes")}>
                         <div className="text-sm pb-2">Class Notes</div>
                         {selectedTypes.includes("class notes") && (
                           <div className="absolute top-4 right-12 text-black text-4xl">
@@ -229,8 +241,7 @@ export default function NotesPage() {
                             ? "border border-black"
                             : ""
                         }`}
-                        onClick={() => handleCheckboxChange("lecture notes")}
-                      >
+                        onClick={() => handleCheckboxChange("lecture notes")}>
                         <div className="text-sm pb-2">Lecture Notes</div>
                         {selectedTypes.includes("lecture notes") && (
                           <div className="absolute top-4 right-12 text-4xl text-black">
@@ -241,17 +252,16 @@ export default function NotesPage() {
                     </div>
                     <button
                       type="submit"
-                      className="bg-[#a883c5] mt-5 w-fit px-5 py-1 rounded-full cursor-pointer flex items-center"
-                    >
-                      <GoArrowDownRight size={25} className="inline" />
-                      <span className="text-[1.25rem] font-semibold">
+                      className="bg-[#a883c5] mt-5 w-fit px-5 py-1 rounded-full cursor-pointer flex items-center">
+                      <GoArrowDownRight size={25} className="inline pb-1" />
+                      <span className="text-[1.25rem] font-[550]">
                         Search Notes
                       </span>
                     </button>
                   </div>
-                  <div className="pt-7 scale-[0.8] w-[22rem]">
+                  <div className="pt-7 scale-[1]">
                     <img
-                      className="w-[22rem] h-[22rem] rounded-[0.75rem]"
+                      className=" rounded-[0.75rem] w-[21rem]"
                       src={shelf}
                       alt="Shelf"
                     />
@@ -264,25 +274,24 @@ export default function NotesPage() {
         </div>
       </div>
 
-      {/* Displaying Filtered Results */}
-      <div className="flex justify-center">
-        <div className="w-[60rem]">
-          <div className={'grid justify-items-center sm:grid-cols-3 grid-cols-2 md:gap-y-14 lg:grid-cols-3 md:gap-x-[1rem] lg:gap-x-3 sm:gap-x-1 mobile:gap-y-10 mobile:gap-x-72 gap-x-[18rem] gap-y-[3rem]'}
-          >
+      <div className="flex justify-center lg:scale-100 md:scale-[0.75] lg:mt-0 md:mt-[-10rem] sm:scale-[0.6] sm:mt-[-15rem] mobile:scale-[0.5] mobile:mt-[-20rem] scale-[0.4] mt-[-20rem]">
+        
+        {/* Notes Display Section */}
+        <div className="min-w-[60rem]">
+          <div className="grid justify-items-center sm:grid-cols-3 grid-cols-2 md:gap-y-14 lg:grid-cols-3 md:gap-x-[1rem] lg:gap-x-3 sm:gap-x-1 mobile:gap-y-10 mobile:gap-x-[10rem] gap-x-[18rem] gap-y-[3rem] ">
             {filteredData.map((item, index) => (
               <a
                 key={item.id}
-                className={'bg-white text-black p-3 rounded-2xl w-[18rem] h-[18rem] md:scale-100 ${theme === "dark" ? "border-0" : "border-2 border-black"}'}
-              >
+                className={`bg-white text-black p-3 rounded-2xl w-[18rem] h-[18rem] md:scale-100 ${
+                  theme === "dark" ? "border-0" : "border-2 border-black"
+                } `}>
                 {/* Component Part */}
                 <div className="flex justify-normal gap-x-4 items-center">
                   {/* Department + Year, Respective School */}
                   <div className="pt-1 pl-2 font-[400] text-lg">
                     {item.department}
                   </div>
-                  <div
-                    className="bg-[#a883c5] px-5 py-0 h-4 rounded-[0.225rem] font-[500] text-sm flex flex-col justify-center"
-                  >
+                  <div className="bg-[#a883c5] px-5 py-0 h-4 rounded-[0.225rem] font-[500] text-sm flex flex-col justify-center">
                     <div className="font-[525]">{item.school}</div>
                   </div>
                 </div>
@@ -291,9 +300,7 @@ export default function NotesPage() {
                   <div className="text-[1.15rem] font-[425] w-[8.7rem]">
                     {item.title}
                   </div>
-                  <div
-                    className="flex justify-normal border-[1.25px] border-black rounded-[0.7rem] w-fit items-center p-2 gap-x-1"
-                  >
+                  <div className="flex justify-normal border-[1.25px] border-black rounded-[0.7rem] w-fit items-center p-2 gap-x-1">
                     <div className="text-sm leading-4">
                       Modules
                       <br /> Covered
@@ -307,14 +314,14 @@ export default function NotesPage() {
                     <img
                       className="w-32 cursor-pointer"
                       src={item.image1}
-                      onClick={() => openImageViewer(0, item)}
+                      onClick={() => openImageViewer(0, item,item.id)}
                     />
                   </div>
                   <div>
                     <img
                       className="w-32 cursor-pointer"
                       src={item.image2}
-                      onClick={() => openImageViewer(1, item)}
+                      onClick={() => openImageViewer(1, item,item.id)}
                     />
                   </div>
                 </div>
@@ -324,29 +331,14 @@ export default function NotesPage() {
         </div>
       </div>
       {isViewerOpen && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80"
-          style={{
-            width: "100vw", // Cover the entire screen width
-            height: "100vh", // Cover the entire screen height
-          }}>
-          <ImageViewer
-            src={images}
-            className="min-w-full"
-            currentIndex={currentImageIndex}
-            disableScroll={true}
-            closeOnClickOutside={true}
-            onClose={closeImageViewer}
-            backgroundStyle={{
-              backgroundColor: "rgba(0, 0, 0, 0.9)", // Dark background
-              zIndex: 9999,
-              width: "100%", // Full width for the background
-              height: "100", // Full height for the background
-            }}
-          />
-        </div>
+        <FullscreenViewer
+          images={images}
+          currentIndex={currentImageIndex}
+          onClose={closeImageViewer}
+          link={data[linkIndex-1].link} // Close the viewer
+          
+        />
       )}
-      {/*The footer part */}
     </div>
   );
 }
