b9deefb00ee148b3f2adccc9deaae880196c891e
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-19 20:57:28 +0530

    Update NoteContext.jsx

--- src/components/common/NoteContext.jsx
+++ src/components/common/NoteContext.jsx
@@ -1,9 +1,21 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const NoteContext = createContext();
 
+export const useNotes = () => useContext(NoteContext);
+
 export function NoteProvider({ children }) {
-  const [notesData, setNotesData] = useState([]);
+
+  const [notesData, setNotesData] = useState(() => {
+    // Retrieve notes from localStorage
+    const storedNotes = localStorage.getItem("notesData");
+    return storedNotes ? JSON.parse(storedNotes) : [];
+  });
+
+  // Save notes to localStorage whenever notesData is updated
+  useEffect(() => {
+    localStorage.setItem("notesData", JSON.stringify(notesData));
+  }, [notesData]);
 
   const addNote = (newNote) => {
     setNotesData((prevNotes) => [...prevNotes, newNote]);
@@ -14,8 +26,4 @@ export function NoteProvider({ children }) {
       {children}
     </NoteContext.Provider>
   );
-}
-
-export function useNotes() {
-  return useContext(NoteContext);
 }
